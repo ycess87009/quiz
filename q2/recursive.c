ca924a45d728d00fba585f12c8397ef795d8766f
ycess87009 <ycess87009@gmail.com>
2015-10-16 16:00:19 +0800

    Finish recursive version for q2

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,22 @@
 /* FIXME: Implement! */
+#include<stdio.h>
+char smallest_character(char str[],char c);
 
 int main()
 {
+    char string[]="abchhqsuvvxz";
+    printf("%c\n",smallest_character(string,'k'));
     return 0;
 }
+
+char smallest_character(char str[],char c)
+{
+    if(str==NULL)return '\0';
+    if(c=='\0')return '\0';
+    if(str[0]=='\0')return str[-1];
+
+    if((int) str[0]>(int) c)return str[0];
+    char test=smallest_character(str+1,c);
+    return test>str[0]? test:str[0];
+}
+
