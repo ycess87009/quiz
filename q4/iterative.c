e8dd3c88f89a43d72dfb8d56828d64be7fda48dc
ycess87009 <ycess87009@gmail.com>
2015-10-16 13:23:59 +0800

    Finish iterative version for q4

--- q4/iterative.c
+++ q4/iterative.c
@@ -4,3 +4,22 @@ int main()
 {
     return 0;
 }
+
+int maxSubArray(int A[],int n){
+    if(n==0)return 0;
+    int temp_max[2],max=A[0],count=0;
+    for(;count<n;count++){
+        if(A[count]<1)continue;
+        temp_max[0]=temp_max[1]=A[count];
+        while(count<(n-1) && (temp_max[1]+A[count+1])>0){
+            temp_max[1]+=A[count+1];
+            if(temp_max[1]>temp_max[0]){
+                temp_max[0]=temp_max[1];
+            }
+            count++;
+        }
+        if(temp_max[0]>max)
+            max=temp_max[0];
+    }
+    return max;
+}
