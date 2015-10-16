8bcf29e27c88accf2355a32c01cb4ea58a98728d
ycess87009 <ycess87009@gmail.com>
2015-10-16 14:13:31 +0800

    Finish iterative version for q5

--- q5/iterative.c
+++ q5/iterative.c
@@ -1,6 +1,40 @@
 /* FIXME: Implement! */
+#define NULL 0
+
+typedef struct ListNode
+{
+    struct ListNode *pNext;
+}ListNode;
 
 int main()
 {
     return 0;
 }
+
+ListNode *detectCycle(ListNode *head)
+{
+    if(head == NULL || head->pNext == NULL)
+    {
+        return NULL;
+    }
+    ListNode *slow=head;
+    ListNode *fast=head;
+    unsigned int count=0;
+    while(fast->pNext!=NULL && fast->pNext->pNext!=NULL)
+    {
+        fast=fast->pNext->pNext;
+        slow=slow->pNext;
+        if(fast == slow)
+        {
+            slow = head;
+            while(slow != fast)
+            {
+                fast = fast->pNext;
+                slow = slow->pNext;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
+
