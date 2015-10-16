79b2da0fa5ef36acc5fb464f8b0075c6ed6d3d06
ycess87009 <ycess87009@gmail.com>
2015-10-16 13:21:40 +0800

    Finish iterative version for q2

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,21 @@
 /* FIXME: Implement! */
+#define NULL 0
+
 
 int main()
 {
+    
     return 0;
 }
+
+char smallest_character(char str[],char c){
+    if(str==NULL)return '\0';
+    if(c==NULL)return '\0';
+    int count=0;
+    while(str[count]!='\0'){
+        if((int) str[count]>(int) c)return str[count];
+        count++;
+    }
+    return str[0];
+}
+
