ec239275d4323ad620d6df41bcd1cc2401aa7f35
ycess87009 <ycess87009@gmail.com>
2015-10-17 21:02:55 +0800

    Finish q4 generate random test data.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,14 +1,60 @@
 /* FIXME: Implement! */
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int maxSubArray(int A[],int n);
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 
 int main()
 {
+    //setup Random number seed
+    srand((unsigned)time(NULL));
+    unsigned int arraysize=rand()%200;
+    /*
+     *generate A[]
+     * */
+    struct timespec start, end;
+    double cpu_time;
+    int count=0;
+    int *A=malloc(arraysize*sizeof(int));
+    for(; count<arraysize; count++) {
+        int pos_nege=rand()%2;
+        int number=rand()%100;
+        if(pos_nege)
+            number=-number;
+        A[count]=number;
+    }
+    /*
+       for(count=0;count<arraysize;count++)
+       {
+       printf("%d,",A[count]);
+       }
+       printf("\n");
+       */
+    clock_gettime(CLOCK_REALTIME, &start);
+    int k=maxSubArray(A,arraysize);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
+    printf("Size: %d ,Result: %d ,Time:%lf sec\n",arraysize,k,cpu_time);
     return 0;
 }
 
 int maxSubArray(int A[],int n)
 {
-    if(n==0)return 0;
+    if(n==0||A==NULL)return 0;
     int temp_max[2],max=A[0],count=0;
     for(; count<n; count++) {
         if(A[count]<1)continue;     //find the first postive integer
